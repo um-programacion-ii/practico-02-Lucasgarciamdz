c4e218b937437c39615f84deb1242bac236de76d
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2024-03-27 17:28:47 -0300

    terminado d

--- src/main/um/entidades/Utensilio.java
+++ src/main/um/entidades/Utensilio.java
@@ -1,10 +1,31 @@
 package src.main.um.entidades;
 
 import src.main.um.interfaces.Despensable;
+import src.main.um.interfaces.Reutilizable;
 
-public class Utensilio implements Despensable {
+public class Utensilio implements Despensable, Reutilizable {
+
+    private int vidaUtil;
+
     @Override
     public String sacar(int cantidad) {
         return null;
+    }
+
+    @Override
+    public int getVidaUtil() {
+        return vidaUtil;
+    }
+
+    @Override
+    public void setVidaUtil(int vidaUtil) {
+        this.vidaUtil = vidaUtil;
+    }
+
+    public Utensilio() {
+    }
+
+    public Utensilio(int vidaUtil) {
+        this.vidaUtil = vidaUtil;
     }
 }
---
+++ src/main/um/interfaces/Reutilizable.java
@@ -0,0 +1,7 @@
+package src.main.um.interfaces;
+
+public interface Reutilizable {
+
+    public int getVidaUtil();
+    public void setVidaUtil(int vidaUtil);
+}
